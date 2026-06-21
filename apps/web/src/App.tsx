bb029ff281bb52aa43ce6acae635ec64902c1b87
acehood455 <acetaiwo8@gmail.com>
2026-06-21 17:12:21 +0100

    fix

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { queryClient } from "./lib/query-client.js";
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import { RootRoute } from "./root.js";
 
